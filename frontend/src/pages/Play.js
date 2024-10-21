394c911c282ddfdb003f73148ef46f3d86834814
Steven Satish <141880871+CodingRadish12@users.noreply.github.com>
2024-10-20 23:12:57 -0500

    switched to custom backend domain

--- frontend/src/pages/Play.js
+++ frontend/src/pages/Play.js
@@ -36,7 +36,7 @@ function Play() {
     const fetchSudokuPuzzle = async () => {
         try {
             const response = await fetch(
-                "https://stevens-backend-sudoku.us-east-2.elasticbeanstalk.com/api/sudoku/generate",
+                "https://api.sudoku-backend.com/api/sudoku/generate/api/sudoku/generate",
                 {
                     method: "GET",
                     headers: {
@@ -79,7 +79,7 @@ function Play() {
         const requestBody = puzzleData; // Puzzle is the state holding the 2D array
         console.log("Sending fetched board:", requestBody); // Log what you're sending
 
-        fetch("https://stevens-backend-sudoku.us-east-2.elasticbeanstalk.com/api/sudoku/solve", {
+        fetch("https://api.sudoku-backend.com/api/sudoku/generate/api/sudoku/solve", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
