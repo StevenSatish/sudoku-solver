55e938dd7ef55bbf1ed8efb3dd1f84f0e3d717b8
Steven Satish <141880871+CodingRadish12@users.noreply.github.com>
2024-10-20 21:31:40 -0500

    updated backend port

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,9 +3,20 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication app = new SpringApplication(BackendApplication.class);
+
+        // Retrieve the PORT environment variable
+        String port = System.getenv("PORT");
+        if (port != null && !port.isEmpty()) {
+            // Set the server port to the PORT environment variable
+            app.setDefaultProperties(Collections.singletonMap("server.port", port));
+        }
+
+        app.run(args);
     }
 }
